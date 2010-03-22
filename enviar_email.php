ccb70db7de3410c30eec6af826bb63a1ac28275f
anaagostinhoricardo <loli_ana@hotmail.com>
2010-03-22 20:08:30 -0300

    Formulario criado com sucesso e mensagem para humanoides

--- enviar_email.php
+++ enviar_email.php
@@ -1,5 +1,40 @@
-<?php
-
-	mail ('loli_ana@hotmail.com', 'Assunto: Teste', 'Apenas um teste');
+<!doctype html>
+<html lang="pt-br">
+<head>
+	<meta charset="utf-8">
+	<title>Kata e-mail</title>
+	<!--[if IE]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+	<style> article, aside, figure, footer, header, hgroup, menu, nav, section { display: block; } </style>
 	
-?>
+</head>
+<body>
+	<div class="container">
+		<?php
+	
+			error_reporting(0);
+			$destinatario='loli_ana@hotmail.com';
+			$assunto=$_POST['Assunto'];
+			$corpo=$_POST['Mensagem'];
+	
+	
+	
+			if (mail ('loli_ana@hotmail.com', 'Assunto: Teste', 'Apenas um teste')):
+	
+	
+	
+		?>
+	
+	
+		<p>Parabéns, e-mail enviado!</p>
+	
+		<?php else: ?>
+	
+		<p>Foi mals...chora...</p>
+		
+		<?php endif; ?>
+		
+	</div>
+</body>
+</html>
+
+
